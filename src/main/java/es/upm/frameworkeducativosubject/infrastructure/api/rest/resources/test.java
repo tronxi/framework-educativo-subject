4a0ed95928c4596c38e3f7689ad694738bdd0bb1
tronxi <sergio.gsanchez97@gmail.com>
2019-11-06 14:34:02 +0100

    add role

--- src/main/java/es/upm/frameworkeducativosubject/infrastructure/api/rest/resources/test.java
+++ src/main/java/es/upm/frameworkeducativosubject/infrastructure/api/rest/resources/test.java
@@ -1,6 +1,7 @@
 package es.upm.frameworkeducativosubject.infrastructure.api.rest.resources;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ public class test {
     @Value("${server.port:entorno por defecto}")
     private String port;
 
-
+    @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String inicio() {
         return "Puerto " + port;
