d8af33f4588975ae70d311ae552bf55ce24cf748
tronxi <sergio.gsanchez97@gmail.com>
2019-11-14 22:45:11 +0100

    json cretor groupDto

--- src/main/java/es/upm/frameworkeducativosubject/infrastructure/api/rest/model/GroupDTO.java
+++ src/main/java/es/upm/frameworkeducativosubject/infrastructure/api/rest/model/GroupDTO.java
@@ -1,14 +1,23 @@
 package es.upm.frameworkeducativosubject.infrastructure.api.rest.model;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Value;
 
-@AllArgsConstructor
-@Data
+@Value
 @Builder
 public class GroupDTO {
     private String id_subject;
     private String id_group;
     private String name;
+
+    @JsonCreator
+    public GroupDTO(@JsonProperty("id_subject") String id_subject,
+                    @JsonProperty("id_group") String id_group,
+                    @JsonProperty("name") String name) {
+        this.id_subject = id_subject;
+        this.id_group = id_group;
+        this.name = name;
+    }
 }
