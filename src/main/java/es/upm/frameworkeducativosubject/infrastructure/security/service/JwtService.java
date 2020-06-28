4ef2a554022ff707ae63e4b673e371c61d400485
tronxi <sergio.gsanchez97@gmail.com>
2020-06-28 17:07:46 +0200

    added token to env

--- src/main/java/es/upm/frameworkeducativosubject/infrastructure/security/service/JwtService.java
+++ src/main/java/es/upm/frameworkeducativosubject/infrastructure/security/service/JwtService.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import es.upm.frameworkeducativosubject.infrastructure.security.exceptions.JwtException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -18,7 +19,8 @@ public class JwtService {
     private static final String USER = "user";
     private static final String ROLES = "roles";
     private static final String ISSUER = "framework-educativo";
-    private static final String SECRET = "clave-secreta-test";
+    @Value("${secret-token}")
+    private static String SECRET;
 
     public boolean isBearer(String authorization) {
         return authorization != null && authorization.startsWith(BEARER) && authorization.split("\\.").length == 3;
